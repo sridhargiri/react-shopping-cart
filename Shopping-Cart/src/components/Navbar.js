c32202989abc50c1a4f60b2c6686ac713cdd1fac
Sridhar S <Sridhargiri1@gmail.com>
2021-05-06 23:43:05 +0530

    first commit

--- Shopping-Cart/src/components/Navbar.js
+++ Shopping-Cart/src/components/Navbar.js
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom'
                     
                     <ul className="right">
                         <li><Link to="/">Shop</Link></li>
+                        <li><Link to="/mouseTracker">Mouse</Link></li>
                         <li><Link to="/cart">My cart</Link></li>
                         <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
                     </ul>
